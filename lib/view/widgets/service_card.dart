3b59c932c5289d697f7b63d9f642fb9f8ccae46a
punkushal <spycymagar@gmail.com>
2025-05-25 10:00:28 +0545

    background img & transparent color added

--- lib/view/widgets/service_card.dart
+++ lib/view/widgets/service_card.dart
@@ -9,55 +9,64 @@ class ServiceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 12),
-      child: Material(
-        color: AppColors.cardbgColor,
-        borderRadius: BorderRadius.circular(16),
-        child: InkWell(
-          onTap: onTap,
-          borderRadius: BorderRadius.circular(16),
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  spacing: 8,
-                  children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(240, 60, 58, 58),
-                        borderRadius: BorderRadius.circular(12),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 14),
+      child: Container(
+        margin: EdgeInsets.only(bottom: 12),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(service.bgImgPath),
+            fit: BoxFit.contain,
+          ),
+        ),
+        child: Material(
+          color: const Color.fromARGB(226, 47, 45, 45),
+          borderRadius: BorderRadius.circular(20),
+          child: InkWell(
+            onTap: onTap,
+            borderRadius: BorderRadius.circular(20),
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    spacing: 8,
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(207, 58, 55, 55),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Image.asset(service.iconPath),
                       ),
-                      child: Image.asset(service.iconPath),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          service.title,
-                          style: TextStyle(
-                            color: AppColors.textPrimary,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            service.title,
+                            style: TextStyle(
+                              color: AppColors.textPrimary,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        Text(
-                          service.description,
-                          style: TextStyle(
-                            color: AppColors.textSecondary,
-                            fontSize: 14,
+                          Text(
+                            service.description,
+                            style: TextStyle(
+                              color: AppColors.textPrimary,
+                              fontSize: 14,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Icon(Icons.play_arrow, color: AppColors.textPrimary),
-              ],
+                        ],
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.play_arrow, color: AppColors.textPrimary),
+                ],
+              ),
             ),
           ),
         ),
