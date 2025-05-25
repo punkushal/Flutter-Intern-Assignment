933d09e3708e66365e4ffba32dc98310e2287a47
punkushal <spycymagar@gmail.com>
2025-05-25 10:01:09 +0545

    provider and home screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_intern_assignment/core/constants/app_colors.dart';
+import 'package:flutter_intern_assignment/view/screens/home_screen.dart';
+import 'package:flutter_intern_assignment/view_models/home_view_model.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => HomeViewModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,10 +27,11 @@ class MyApp extends StatelessWidget {
       title: 'S.Rocks.Music',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: AppColors.primaryPink,
+        primaryColor: AppColors.primaryRed,
         scaffoldBackgroundColor: AppColors.bgColor,
         textTheme: GoogleFonts.interTextTheme(),
       ),
+      home: HomeScreen(),
     );
   }
 }
